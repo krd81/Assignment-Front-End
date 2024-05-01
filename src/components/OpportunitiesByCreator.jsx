05831c31ef18d172caf53c23bc9483d237ba81b2
Kelly <kelly.degannes@gmail.com>
2024-05-01 18:01:30 +1000

    Added additional styling to ManageListings

--- src/components/OpportunitiesByCreator.jsx
+++ src/components/OpportunitiesByCreator.jsx
@@ -3,91 +3,92 @@ import { useState, useEffect, useContext } from "react"
 import IonIcon from '@reacticons/ionicons'
 import { useNavigate } from 'react-router-dom'
 import { AppContext } from '../authentication/AppContext'
+import '../assets/css/Opportunities.css'
 
 
 export const OpportunitiesByCreator = () => {
-    // App level state objects
-    const { allListings, listing, loggedInUser } = useContext(AppContext)
-    const [listings, setListings] = allListings
-    const [currentListing, setCurrentListing] = listing
-    const [currentUser, setCurrentUser] = loggedInUser
+  // App level state objects
+  const { allListings, listing, loggedInUser } = useContext(AppContext)
+  const [listings, setListings] = allListings
+  const [currentListing, setCurrentListing] = listing
+  const [currentUser, setCurrentUser] = loggedInUser
 
-    // Local state object
-    let [userListings, setUserListings] = useState([])
-    console.log(currentUser)
-    console.log(currentUser._id)
-    console.log(listings)
-    console.log(listings[0].creator._id)
+  // Local state object
+  let [userListings, setUserListings] = useState([])
+  console.log(currentUser)
+  console.log(currentUser._id)
+  console.log(listings)
+  console.log(listings[0].creator._id)
 
 
-    useEffect(() => {
-        let results = []
-        for (let listing of listings) {
-            if (listing && listing.creator) {
-              if (listing.creator._id === currentUser._id) {
-                console.log("Listing creator id matches user's id")
-                results.push(listing)
-              } else {
-              console.log("Unable to tell if id's match")
-              }
-            }
+  useEffect(() => {
+    let results = []
+    for (let listing of listings) {
+      if (listing && listing.creator) {
+        if (listing.creator._id === currentUser._id) {
+          console.log("Listing creator id matches user's id")
+          results.push(listing)
+        } else {
+          console.log("Unable to tell if id's match")
         }
-        setUserListings(results)
-    }, [setUserListings, listing, listings, currentUser])
-
-
-    const nav = useNavigate()
-
-    const formatDate = (dateToFormat) => {
-      const date = new Date(dateToFormat);
-      const dd = date.toISOString().slice(8,10);
-      const mm = date.toISOString().slice(5,7);
-      const yy = date.toISOString().slice(2,4);
-
-      return `${dd}/${mm}/${yy}`
-    }
-
-    const handleView = (listing) => {
-        // Open Listing in view mode
-        nav(`/listings/creator/${listing._id}`)
-        setCurrentListing(listing)
-    }
-
-    const handleEdit = (listing) => {
-        // Open Listing in edit mode
-        // Navigates to 'NewListing' component
-        nav(`/listings/creator/${listing._id}/edit`)
-        setCurrentListing(listing)
-      }
-
-    const handleDelete = (listing) => {
-      if (confirm("This listing will be deleted. Are you sure?")) {
-         // Delete Listing
-          deleteListing(listing._id)
-          // Update userListing and global listings state variables
-          setUserListings(prevUserListings => prevUserListings.filter((prevUserListing) => prevUserListing !== listing))
-          setListings(prevListings => prevListings.filter((prevListing) => prevListing !== listing))
-          // Re-load listings excl the deleted listing
-          nav(`/opportunities/${currentUser._id}`)
       }
     }
+    setUserListings(results)
+  }, [setUserListings, listing, listings, currentUser])
 
-    const deleteListing = async (listingID) => {
-      try {
-        // const response = await fetch('https://talent-forge-api-atu2.onrender.com/listings', {
-        await fetch(`http://localhost:8002/listings/${listingID}`, {
-          method: 'DELETE',
-          headers: {
-            'Authorization': `Bearer ${sessionStorage.getItem('token')}`
-          }
-        })
-      } catch (error) {
-        console.error('Failed to delete listing:', error)
-      }
+
+  const nav = useNavigate()
+
+  const formatDate = (dateToFormat) => {
+    const date = new Date(dateToFormat);
+    const dd = date.toISOString().slice(8,10);
+    const mm = date.toISOString().slice(5,7);
+    const yy = date.toISOString().slice(2,4);
+
+    return `${dd}/${mm}/${yy}`
+  }
+
+  const handleView = (listing) => {
+    // Open Listing in view mode
+    nav(`/listings/creator/${listing._id}`)
+    setCurrentListing(listing)
+  }
+
+  const handleEdit = (listing) => {
+    // Open Listing in edit mode
+    // Navigates to 'NewListing' component
+    nav(`/listings/creator/${listing._id}/edit`)
+    setCurrentListing(listing)
+  }
+
+  const handleDelete = (listing) => {
+    if (confirm("This listing will be deleted. Are you sure?")) {
+      // Delete Listing
+      deleteListing(listing._id)
+      // Update userListing and global listings state variables
+      setUserListings(prevUserListings => prevUserListings.filter((prevUserListing) => prevUserListing !== listing))
+      setListings(prevListings => prevListings.filter((prevListing) => prevListing !== listing))
+      // Re-load listings excl the deleted listing
+      nav(`/opportunities/${currentUser._id}`)
     }
+  }
+
+  const deleteListing = async (listingID) => {
+    try {
+      // const response = await fetch('https://talent-forge-api-atu2.onrender.com/listings', {
+      await fetch(`http://localhost:8002/listings/${listingID}`, {
+        method: 'DELETE',
+        headers: {
+          'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+        }
+      })
+    } catch (error) {
+      console.error('Failed to delete listing:', error)
+    }
+  }
 
 
-    return (
+  return (
         <>
             <div className="bg-blue-50 mx-6 my-6 md:my-12 lg:my-24 p-6 md:p-10 lg:p-16 xl:mx-40">
                 <div className="grid grid-cols-1  gap-4">
@@ -99,7 +100,7 @@ export const OpportunitiesByCreator = () => {
                     <div className="grid grid-cols-1 gap-6">
                       {/* <div className="flex-row"> */}
             {userListings.map((listing) => (
-              <div key={listing._id} className={`${listing.listingActive ? 'bg-white' : 'bg-gray-300'} max-w-full overflow-hidden shadow-lg rounded-lg border`}>
+              <div key={listing._id} className={`${listing.listingActive ? 'bg-white' : 'bg-gray-300'} max-w-full overflow-hidden shadow-lg rounded-lg border select-list-item`}>
                 <div className="p-4 md:flex-row ">
                   {/* onClick={() => {
                     handleView(listing)
@@ -115,32 +116,27 @@ export const OpportunitiesByCreator = () => {
                   </div>
                   </div>
 
-                  <span className='flex md:flex justify-evenly md:justify-evenly text-gray-500 md:text-xl'>
+                  <span className='flex md:flex justify-evenly md:justify-evenly text-gray-500  md:text-xl'>
                     {/* View Listing */}
-                    <a className="md:p-1"
+                    <a className="md:p-1 hover:text-blue-700"
                     onClick={() => {
                       handleView(listing)
                       }}>
                       <IonIcon name="eye-outline" size="large" />
-
                     </a>
                     {/* Edit Listing */}
-                    <a className="md:p-1"
+                    <a className="md:p-1 hover:text-blue-700"
                     onClick={() => {
                       handleEdit(listing)
                       }}>
-
                       <IonIcon name="brush-outline" size="large" />
-
                     </a>
                     {/* Delete Listing */}
-                    <a className="md:p-1"
+                    <a className="md:p-1 hover:text-blue-700"
                     onClick={() => {
                       handleDelete(listing)
                       }}>
-
                       <IonIcon name="trash-outline" size="large" />
-
                     </a>
 
 
@@ -150,12 +146,11 @@ export const OpportunitiesByCreator = () => {
               </div>
             ))}
           </div>
-
                 </div>
             </div>
 
         </>
-    )
+  )
 }
 
 export default OpportunitiesByCreator
