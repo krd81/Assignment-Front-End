228bae38fdf428ff76960986eaeda3e2fb8f7faa
Kelly <kelly.degannes@gmail.com>
2024-05-07 12:06:41 +1000

    Added ability for users to view their applications

--- src/components/UserAssetsList.jsx
+++ src/components/UserAssetsList.jsx
@@ -1,17 +1,14 @@
-import React, { useContext } from 'react'
+import { useContext } from 'react'
 import { AppContext } from '../authentication/AppContext'
-import { useNavigate, useLocation, useParams } from "react-router-dom"
+import { useLocation, Link } from "react-router-dom"
 
 // User Assets - reusable component for Applications / Events etc
 const UserAssetsList = () => {
-    const { allListings, loggedInUser } = useContext(AppContext)
-    const [ listings ] = allListings
+    const { listing , loggedInUser } = useContext(AppContext)
+    const [ currentListing, setCurrentListing ] = listing
     const [ currentUser ] = loggedInUser
     let asset = '';
-    const params = useParams();
     const location = useLocation();
-    console.log(params)
-    console.log(location)
 
     function assetType () {
       const pathname = location.pathname
@@ -25,18 +22,28 @@ const UserAssetsList = () => {
     function showAssets () {
       switch (asset) {
         case 'Applications':
-          for (let listing in currentUser.applications) {
+          for (let listing of currentUser.applications) {
             return (
-              `<li>
-                ${listing.title}
-              </li>`
+              <li className="list-disc">
+                <Link to={`/listings/${listing._id}`}>
+                  {listing.title}
+                  {setCurrentListing(listing)}
+                </Link>
+              </li>
+            )
+          }
+          if (currentUser.applications.length < 1) {
+            return (
+              <p className="text-center mx-6 text-xl md:text-4xl">
+                No {asset.toLowerCase()} found
+              </p>
             )
           }
         break;
         default:
-          return (
-            `<p>No ${asset.toLowerCase()} found</p>`
-          )
+          // return (
+            // Direct to 404 not found
+          // )
       }
     }
 
@@ -44,15 +51,17 @@ const UserAssetsList = () => {
     document.title = asset
     return (
         <>
-          <div className="bg-blue-50 mx-6 my-6 md:my-12 lg:my-24 p-6 md:p-10 lg:p-16 xl:mx-96">
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          <div className="md:col-span-3 flex items-center justify-center">
-          <h2 className="text-3xl font-semibold mt-8 mb-4">{`Your ${asset}`}</h2>
-          <ul>
-            {showAssets()}
-          </ul>
-          </div>
-          </div>
+          <div className="bg-blue-50 mx-6 md:mx-14 lg:mx-48 my-6 md:my-8 lg:my-8 p-6 md:p-10 lg:p-16 min-h-screen">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+              <div className="md:col-span-3 flex-row items-center justify-center">
+                <h2 className="text-3xl md:text-5xl lg:text-5xl text-center font-semibold mt-8 mb-4">{`Your ${asset}`}</h2>
+                <div className="py-3 md:py-10 lg:py-14 mx-6 text-xl md:text-4xl">
+                  <ul>
+                    {showAssets()}
+                  </ul>
+                </div>
+              </div>
+            </div>
           </div>
 
         </>
