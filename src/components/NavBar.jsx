b02ced07abaf9ff8673e43f99777e048b925463d
Kelly <kelly.degannes@gmail.com>
2024-05-27 22:35:46 +1000

    Implemented nested menu on nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -172,6 +172,106 @@ export default function NavBar() {
                           </a>
                         )}
                       </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Menu as="div" className="relative">
+                            <Menu.Button className={classNames(
+                              active ? "bg-gray-100" : "",
+                              "block w-full text-left px-4 py-2 text-lg text-black cursor-pointer"
+                            )}>
+                              Admin: User Control
+                            </Menu.Button>
+                            <Transition
+                              as={Fragment}
+                              enter="transition ease-out duration-100"
+                              enterFrom="transform opacity-0 scale-95"
+                              enterTo="transform opacity-100 scale-100"
+                              leave="transition ease-in duration-75"
+                              leaveFrom="transform opacity-100 scale-100"
+                              leaveTo="transform opacity-0 scale-95"
+                            >
+                              <Menu.Items className="absolute right-full top-0 z-10 mt-0 w-48 origin-top-left rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                <Menu.Item>
+                                  {({ active }) => (
+                                    <a
+                                      href="/user-new"
+                                      className={classNames(
+                                        active ? "bg-gray-100" : "",
+                                        "block px-4 py-2 text-lg text-black cursor-pointer"
+                                      )}
+                                    >
+                                      Create User
+                                    </a>
+                                  )}
+                                </Menu.Item>
+                                <Menu.Item>
+                                  {({ active }) => (
+                                    <a
+                                      href="/user-edit"
+                                      className={classNames(
+                                        active ? "bg-gray-100" : "",
+                                        "block px-4 py-2 text-lg text-black cursor-pointer"
+                                      )}
+                                    >
+                                      Edit User
+                                    </a>
+                                  )}
+                                </Menu.Item>
+                              </Menu.Items>
+                            </Transition>
+                          </Menu>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <Menu as="div" className="relative">
+                            <Menu.Button className={classNames(
+                              active ? "bg-gray-100" : "",
+                              "block w-full text-left px-4 py-2 text-lg text-black cursor-pointer"
+                            )}>
+                              Admin: Listings Control
+                            </Menu.Button>
+                            <Transition
+                              as={Fragment}
+                              enter="transition ease-out duration-100"
+                              enterFrom="transform opacity-0 scale-95"
+                              enterTo="transform opacity-100 scale-100"
+                              leave="transition ease-in duration-75"
+                              leaveFrom="transform opacity-100 scale-100"
+                              leaveTo="transform opacity-0 scale-95"
+                            >
+                              <Menu.Items className="absolute right-full top-0 z-10 mt-0 w-48 origin-top-left rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                <Menu.Item>
+                                  {({ active }) => (
+                                    <a
+                                      href="/listing-new"
+                                      className={classNames(
+                                        active ? "bg-gray-100" : "",
+                                        "block px-4 py-2 text-lg text-black cursor-pointer"
+                                      )}
+                                    >
+                                      Create Listing
+                                    </a>
+                                  )}
+                                </Menu.Item>
+                                <Menu.Item>
+                                  {({ active }) => (
+                                    <a
+                                      href="/listing-edit"
+                                      className={classNames(
+                                        active ? "bg-gray-100" : "",
+                                        "block px-4 py-2 text-lg text-black cursor-pointer"
+                                      )}
+                                    >
+                                      Edit Listing
+                                    </a>
+                                  )}
+                                </Menu.Item>
+                              </Menu.Items>
+                            </Transition>
+                          </Menu>
+                        )}
+                      </Menu.Item>
 
                       <Menu.Item>
                         {({ active }) => (
