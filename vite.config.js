8c09159f7c72a5bbbe378c5a7db9e3ba09556a76
Kelly <kelly.degannes@gmail.com>
2024-06-21 20:40:54 +1000

    Updated imports for background image

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,15 @@ export default defineConfig({
   },
   plugins: [react()],
   build: {
-    assetsDir: 'assets'
-  }
-})
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  },
+});
