6c625448ee987a95b9e5075bb6279e2f48907758
Kelly <kelly.degannes@gmail.com>
2024-06-22 21:01:13 +1000

    Updated 404-error component

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -19,7 +19,7 @@ const Error = () => {
         <div className="bg-404error bg-cover bg-bottom min-h-screen pb-20">
             <div className="flex justify-center items-center w-full h-screen">
                 <div className='block'>
-                    <h1 className="text-[18rem] text-blue-100 text-center" >404<br /></h1>
+                    <h1 className="text-[18rem] text-blue-100 text-center">404<br /></h1>
                     <h1 className="text-5xl text-blue-100 text-center">Something weird has happened</h1>
                     <div className='flex justify-center pt-12'>
                         <button className="bg-dark-blue text-white text-lg md:text-xl lg:text-lg p-4 rounded-lg shadow-md m-1 w-56 hover:bg-white hover:text-dark-blue"
@@ -34,9 +34,7 @@ const Error = () => {
                         </button>
                     </div>
                 </div>
-
             </div>
-
         </div>
     </>
   )
